dab47afe49adc6ccc30754bd8d7ce1fa865523b3
giddy11 <edoghotugiddy@gmail.com>
2022-05-08 21:12:38 +0100

    Add project files.

---
+++ GenericsLastItem/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+static void Reverse<T>(T[] inputArray)
+{
+    for (int i = inputArray.Length - 1; i >= 0; i--)
+    {
+        Console.WriteLine($"The last item is {inputArray[i]} ");
+        break;
+    }
+    for (int i = inputArray.Length - 1; i >= 0; i--)
+    {
+        Console.WriteLine($"Total count of array items is:{i}");
+        break;
+    }
+    Console.WriteLine();
+}
+
+string[] names = { "Gideon", "Dozie", "Victory" };
+int[] numbers = { 1, 2, 3, 4, 5 };
+
+
+Reverse(names);
+Reverse(numbers);
+
+Console.ReadLine();
+
---
+++ GenericsReverseArray/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+static void Reverse<T>(T[] inputArray)
+{
+    for (int i = inputArray.Length - 1; i >= 0; i--)
+    {
+        Console.WriteLine($"{inputArray[i]} ");
+    }
+}
+
+string[] names = { "Gideon", "Dozie", "Victory" };
+int[] numbers = { 1, 2, 3, 4, 5 };
+
+
+Reverse(names);
+Reverse(numbers);
+
+Console.ReadLine();
